5b088e15832ea8b0f4e6c086c58b9aa809125fd3
hongmunoa <hongmunoa@gmail.com>
2025-12-01 14:44:58 +0900

    사용자 기능에 명령어 추가

--- lib/Factory/UserGroupFactory.php
+++ lib/Factory/UserGroupFactory.php
@@ -813,7 +813,17 @@ class UserGroupFactory extends BaseFactory
                 'command.view' => [
                     'feature' => 'command.view',
                     'group' => 'displays',
-                    'title' => __('Page to view/add/edit/delete Commands')
+                    'title' => __('Page to view Commands')
+                ],
+                'command.add' => [
+                    'feature' => 'command.add',
+                    'group' => 'displays',
+                    'title' => __('Include "Add Command" button to allow for additional Commands to be added to the platform')
+                ],
+                'command.modify' => [
+                    'feature' => 'command.modify',
+                    'group' => 'displays',
+                    'title' => __('Allow edits including deletion for all created Commands')
                 ],
                 'display.syncView' => [
                     'feature' => 'display.syncView',
