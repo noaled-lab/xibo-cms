b50b102a0c1e06a341fc3ad2db665f783583f08f
hongmunoa <hongmunoa@gmail.com>
2025-12-05 11:39:48 +0900

    디스플레이 비우기 권한 변경

--- lib/Controller/Display.php
+++ lib/Controller/Display.php
@@ -3142,7 +3142,7 @@ class Display extends Base
     {
         $display = $this->displayFactory->getById($id);
 
-        if (!$this->getUser()->checkViewable($display) || !$this->getUser()->isSuperAdmin()) {
+        if (!$this->getUser()->checkViewable($display) || !$this->getUser()->featureEnabled('displays.modify')) {
             throw new AccessDeniedException();
         }
 
@@ -3190,7 +3190,7 @@ class Display extends Base
     {
         $display = $this->displayFactory->getById($id);
 
-        if (!$this->getUser()->checkViewable($display) || !$this->getUser()->isSuperAdmin()) {
+        if (!$this->getUser()->checkViewable($display) || !$this->getUser()->featureEnabled('displays.modify')) {
             throw new AccessDeniedException();
         }
 
