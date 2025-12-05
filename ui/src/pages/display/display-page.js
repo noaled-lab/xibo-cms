e11d67d1b17f29c93f69848bc78e19a4c1f8f24d
hongmunoa <hongmunoa@gmail.com>
2025-12-05 09:43:40 +0900

    디스플레이 컬럼 수정

--- ui/src/pages/display/display-page.js
+++ ui/src/pages/display/display-page.js
@@ -55,7 +55,7 @@ $(function() {
     columns: [
       {data: 'displayId', responsivePriority: 2},
       {data: 'display', responsivePriority: 2},
-      {data: 'displayType', responsivePriority: 2},
+      {data: 'displayType', visible: false, responsivePriority: 2},
       {data: 'address', visible: false, responsivePriority: 5},
       {
         data: 'mediaInventoryStatus',
@@ -85,9 +85,9 @@ $(function() {
       },
       {
         data: 'currentLayout',
-        visible: false,
+        visible: true,
         sortable: false,
-        responsivePriority: 5,
+        responsivePriority: 3,
       },
       {
         data: 'storageAvailableSpace',
