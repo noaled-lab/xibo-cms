1c3a10bb030c8a96d3623dc2feb15f4eccc6bee3
hongmunoa <hongmunoa@gmail.com>
2026-01-12 16:29:29 +0900

    RequiredFiles bytesDownloaded 필드 추가 및 MediaInventory API 수정

--- lib/Factory/RequiredFileFactory.php
+++ lib/Factory/RequiredFileFactory.php
@@ -38,6 +38,7 @@ class RequiredFileFactory extends BaseFactory
 
     private $hydrate = [
         'intProperties' => ['bytesRequested', 'complete'],
+        'doubleProperties' => ['bytesDownloaded'],
         'stringProperties' => ['realId'],
     ];
 
